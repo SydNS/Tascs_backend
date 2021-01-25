ac437d17887a8a6cc18ea3f769677339cad0ced7
SydNS <sydneynsiimbe@gmail.com>
2021-01-24 19:53:54 -0800

    removed the not allowed formt of writing paths in the url

--- tascsapi/restapi/urls.py
+++ tascsapi/restapi/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from . import views
+from .views import TaskViewSet
 
+router = routers.DefaultRouter()
+router.register('', TaskViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
 ]
