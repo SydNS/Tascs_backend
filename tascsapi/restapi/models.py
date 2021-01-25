f21fcf629af09b0150b7a6cd3e1571a3c15c2c3f
SydNS <sydneynsiimbe@gmail.com>
2021-01-24 20:11:33 -0800

    created a user model that will be used to create token for the respective users and also this posts username and email to the restframewrk-tokensystem to provide token for the user

--- tascsapi/restapi/models.py
+++ tascsapi/restapi/models.py
@@ -11,3 +11,23 @@ class Tasks(models.Model):
     created_by = models.CharField(max_length=50, blank=True, null=True)
     priority = models.IntegerField(validators=[MinValueValidator(1),
                                                MaxValueValidator(5)])
+
+class TascsUAccount(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    lastname = models.CharField(max_length=20, unique=True)
+    Uemail = models.EmailField(max_length=30, unique=True)
+    password = models.CharField(max_length=20)
+    residence = models.CharField(max_length=30)
+    country_of_origin = models.CharField(max_length=30)
+    reg_date = models.DateTimeField(default=timezone.now)
+    # photo = models.ImageField(upload_to="images/%Y/%b/%a/%d/%H/%M", null=True, blank=False)
+
+
+    class Meta:
+        ordering = ('reg_date',)
+
+    def __str__(self):
+        name = str(self.name).upper()
+        name2 = str(self.lastname).upper()
+        Uname = name + ' ' + name2
+        return Uname
