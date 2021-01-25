ff8bce158764cb0cdc9a30de09e2a4b287b00dd4
SydNS <sydneynsiimbe@gmail.com>
2021-01-24 18:55:19 -0800

    created models for the tasks

--- tascsapi/restapi/models.py
+++ tascsapi/restapi/models.py
@@ -1,3 +1,13 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+# Models
+class Tasks(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.CharField(max_length=50, blank=True, null=True)
+    priority = models.IntegerField(validators=[MinValueValidator(1),
+                                               MaxValueValidator(5)])
