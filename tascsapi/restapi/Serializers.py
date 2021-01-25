b1556fa940f21a117af54465ce2138a8b24f6f0e
SydNS <sydneynsiimbe@gmail.com>
2021-01-24 18:59:59 -0800

    from note to task

--- tascsapi/restapi/Serializers.py
+++ tascsapi/restapi/Serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Tasks
 
 
-class NoteSerialiser(serializers.HyperlinkedModelSerializer):
+class TaskSerialiser(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tasks
         fields = ('id', 'title', 'description', 'created_at', 'created_by', 'priority')
